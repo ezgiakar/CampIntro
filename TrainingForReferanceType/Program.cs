a9ec4ff42722ba1b039ff95465f9c97353b2b911
Ezgi Akar <ezgiakar.ea1@gmail.com>
2021-01-22 02:43:01 +0300

    22.01.2021

---
+++ TrainingForReferanceType/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace TrainingForReferanceType
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person1 = new Person();
+            person1.FirstName = "Ezgi";
+
+            Person person2 = new Person();
+            person2.FirstName = "Beyza";
+
+            person1 = person2;
+
+            person2.FirstName = "Ceren";
+
+            Customer customer = new Customer();
+            customer.FirstName = "Mine";
+
+            Person person3 = new Person();
+            person3.FirstName = "Lale";
+            person3 = customer;
+
+            Employee employee = new Employee();
+            employee.FirstName = "Nurtaç";
+
+            person3 = employee;
+
+
+            Console.WriteLine("Person3 : " + person3.FirstName);
+            Console.WriteLine("Person3 : " + person3.FirstName);
+            Console.WriteLine("Person1 = " + person1.FirstName);
+        }
+    }
+
+    class Person
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+
+    class Customer: Person
+    {
+        public int CreditCard { get; set; }
+    }
+
+    class Employee: Person //inheritance
+    {
+        public double Salary { get; set; }
+    }
+}
