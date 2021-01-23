fc3a6540d6b57fee4e14ad7b96529bb06eeab5c3
Ezgi Akar <ezgiakar.ea1@gmail.com>
2021-01-23 12:24:15 +0300

    23.01.2021

---
+++ MyDictionary/DictionaryHomework.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class DictionaryHomework<TKey, TValue>
+    {
+        TKey[] keys;
+        TValue[] values;
+        TKey[] tempKeys;
+        TValue[] tempValues;
+
+        public DictionaryHomework()
+        {
+            keys = new TKey[0];
+            values = new TValue[0];
+        }
+        public void Add(TKey key, TValue value)
+        {
+            TKey[] tempKey = keys;
+            TValue[] tempValues = values;
+
+            keys = new TKey[keys.Length + 1];
+            values = new TValue[values.Length + 1];
+
+            for (int i = 0; i < tempKey.Length; i++)
+            {
+                keys[i] = tempKey[i];
+                values[i] = tempValues[i];
+            }
+
+            keys[keys.Length - 1] = key;
+            values[values.Length - 1] = value;
+        }
+
+        public void GetAll()
+        {
+
+            for (int i = 0; i < keys.Length; i++)
+            {
+                Console.WriteLine(keys[i] + " " + values[i]);
+            }
+
+        }
+
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DictionaryHomework<int, int> cars = new DictionaryHomework<int, int>();
+            cars.Add(34, 65000);
+            cars.Add(35, 85000);
+            cars.Add(41, 55000);
+
+            cars.GetAll();
+        }
+    }
+}
