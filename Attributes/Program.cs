346f7e2493c98c8e8ca793fed2349b16b57cf31a
Ezgi Akar <ezgiakar.ea1@gmail.com>
2021-01-29 22:59:50 +0300

    29.01.2021

---
+++ Attributes/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Attributes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer = new Customer { Id = 1, LastName = "Ezgi", Age = 24 };
+            CustomerDal customerDal = new CustomerDal();
+            customerDal.Add(customer);
+            Console.ReadLine();
+        }
+    }
+    // attribute iş kurallarını koymamızı sağlar.
+    [ToTable("Customers")]
+    class Customer
+    {
+        public int Id { get; set; }
+        [RequiredProperty]
+        public string FirstName { get; set; }
+        [RequiredProperty]
+        public string LastName { get; set; }
+        [RequiredProperty]
+        public int Age { get; set; }
+    }
+
+    class CustomerDal
+    {
+        [Obsolete("Don't use Add, instead use AddNew Method")] // hazır attribute. yeni add yazdık o nedenle uyarı için yazarız.
+        public void Add(Customer customer)
+        {
+            Console.WriteLine("{0},{1},{2},{3} added",
+                customer.Id, customer.FirstName, customer.LastName, customer.Age);
+        }
+
+        public void AddNew(Customer customer)
+        {
+            Console.WriteLine("{0},{1},{2},{3} added",
+                customer.Id, customer.FirstName, customer.LastName, customer.Age);
+        }
+    }
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field, AllowMultiple = true)]
+    class RequiredPropertyAttribute: Attribute
+    {
+
+    }
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
+    class ToTableAttribute : Attribute
+    {
+        private string _tableName;
+
+        public ToTableAttribute(string tableName)
+        {
+            _tableName = tableName;
+        }
+    }
+}
---
+++ Reflection/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reflection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //diyelim ki çalışma anında yapmak istiyoruz. o zaman reflection kullanırız.
+            //Math math = new Math(2,3); aşağıdaki activator ile aynıdır. Gelen tipe göre örnek yapmış oluruz.
+            //Console.WriteLine( math.Sum1() );
+            //Console.WriteLine( math.Sum(3,4) ); 
+
+            var type = typeof(Math);
+
+            Math math = (Math)Activator.CreateInstance(type, 6, 7);
+            math.Sum(4, 5);
+
+            Console.ReadLine();
+        }
+    }
+
+    public class Math
+    {
+        int _sayi1;
+        int _sayi2;
+
+        public Math(int sayi1, int sayi2)
+        {
+            _sayi1 = sayi1;
+            _sayi2 = sayi2;
+        }
+        
+        public int Sum(int sayi1, int sayi2)
+        {
+            return sayi1 + sayi2;
+        }
+
+        public int Carp(int sayi1, int sayi2)
+        {
+            return sayi1 * sayi2;
+        }
+
+        public int Sum1()
+        {
+            return _sayi1 + _sayi2;
+        }
+
+        public int Carp2()
+        {
+            return _sayi1 * _sayi2;
+        }
+
+    }
+}
